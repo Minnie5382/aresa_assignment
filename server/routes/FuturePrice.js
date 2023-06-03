393e26151a9e0a2f20c1dbf16b1a9a25a5cc9b07
Minn <97179789+Minnie5382@users.noreply.github.com>
2023-06-04 01:29:05 +0900

    add models

--- server/routes/FuturePrice.js
+++ server/routes/FuturePrice.js
@@ -2,10 +2,13 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/',(req, res) => { 
-    res.send("get localhost:8080/aresa-api/future_price")
+    const body = req.body;
+    console.log(body);
+    res.send("get localhost:8080/aresa-api/future_price");
+    console.log(body);
 })
-router.post('/',(req,res) => {
+router.post('/',(req,res) => { 
     res.send('post localhost:8080/aresa-api/future_price')
 })
 
-    module.exports = router;
+module.exports = router;
--- server/server.js
+++ server/server.js
@@ -1,17 +1,9 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
 port = 8080;
 
-const mysql = require('mysql');  // mysql 모듈 로드
-const conn = {  // mysql 접속 설정
-    host: 'localhost',
-    port: '3306',
-    user: 'root',
-    password: 'shameonme',
-    database: 'aresa-apart'
-};
-
-
+app.use(bodyParser.json());
 
 const historicalPriceRoutes = require("./routes/HistoricalPrice");
 const futurePriceRoutes = require("./routes/FuturePrice");
@@ -19,7 +11,20 @@ const futurePriceRoutes = require("./routes/FuturePrice");
 app.use("/aresa-api/historical_price", historicalPriceRoutes);
 app.use("/aresa-api/future_price", futurePriceRoutes);
 
+
 app.listen(port, () => {
     console.log(`listening on ${port}`)
 })
 
+// // POST historical_price
+// app.post('/aresa-api/historical_price/:aptId', (req, res) => {
+//     const aptId = req.params.aptId;
+//     const body = req.body
+
+//     res.send(body)
+// })
+
+// // GET historical_price
+// app.get('/aresa-api/historical_price/:aptId', (req, res) => {
+//     console.log(req.body);
+// })
