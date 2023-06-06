1b2772f09b9ec52aca0dfed97b2652231c35ca46
Minn <97179789+Minnie5382@users.noreply.github.com>
2023-06-07 05:25:25 +0900

    model 구성 수정

--- server/models/GetPrices.js
+++ server/models/GetPrices.js
@@ -23,5 +23,20 @@ module.exports = class GetPrices {
     })
   }
 
+  getHistoricalPrices(callback) {
+    const postHisPriceQuery = 'select Apartment.aptId, Apartment.aptName, year, month, HistoricalPrice.price\n'+
+    'from HistoricalPrice\n'+
+    'join Apartment on HistoricalPrice.aptId = Apartment.aptId\n'+
+    'where Apartment.aptId = ? and Apartment.status = "ACTIVE" and HistoricalPrice.status = "ACTIVE" and'+
+    'year = ? and month >= ? and month <= ?;';
+    
+    const postHisPriceParams = [this.aptId, this.year, this.monthStart, this.monthEnd];
+
+    db.query(postHisPriceQuery, postHisPriceParams, (err, res) => {
+        if (err) throw err;
+        callback(res);
+    })
+  }
+
 
 }
